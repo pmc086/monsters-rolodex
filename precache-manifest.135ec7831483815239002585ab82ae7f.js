e1066ebd1f084efd04612b1731958e1a870afda5
Paul McManus <paul.mcmanus@live.com.au>
2020-01-29 22:04:09 +1000

    Updates

---
+++ precache-manifest.135ec7831483815239002585ab82ae7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3d2204ac39978a72e5d022c0adce88",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1fcb033fc3bdb8aea44a",
+    "url": "/monsters-rolodex/static/css/main.018312af.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/monsters-rolodex/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fcb033fc3bdb8aea44a",
+    "url": "/monsters-rolodex/static/js/main.db4f5677.chunk.js"
+  },
+  {
+    "revision": "76dff06d2def61fa221b",
+    "url": "/monsters-rolodex/static/js/runtime-main.0d1dab6d.js"
+  }
+]);
